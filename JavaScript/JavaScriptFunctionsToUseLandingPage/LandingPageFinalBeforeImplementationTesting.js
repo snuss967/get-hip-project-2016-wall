451a1ff4a3e0eb6109e26ed2572dec921b1c65ed
Spencer Nussrallah <spencer.nussrallah@gmail.com>
2016-02-19 21:06:17 -0600

    Pushed From My Hotel Rooms, includes a more developed drag and drop needs to be finished and has not yet been tested. And edit for Notes. Feel Free to modify this code as much as you want

--- JavaScript/JavaScriptFunctionsToUseLandingPage/LandingPageFinalBeforeImplementationTesting.js
+++ JavaScript/JavaScriptFunctionsToUseLandingPage/LandingPageFinalBeforeImplementationTesting.js
@@ -1,3 +1,12 @@
+//This Code is not guaranteed to not not break. It is merely a starting point right now which is why I have pushed it so early. It needs to be refined.
+//Most of these functions have been tried in very limited enviornments so they do work but no guarantees that they will work with our product exactly
+//In this code there are many places where it needs to be linked up with HTML and CSS that I have not implementated because I do not have access to the
+//front end designs at this point. Use this code however you like I still have a copy in my private repo. If you need me dont hesitate to get ahold of me
+//by phone 4029177931. Pushed from my Hotel Room
+
+//for our sort function can we return date time in TimeStamp format? We will have then have it already converted to seconds/miliseconds and can sort biggest to largest
+//largest to smallest
+
 //This will be our list of functions that interact with the HTML Page it pieces together other functions found in the JavaScript
 //Folder. The functions here include, The Initial Load, Create, Delete, Edit, Sort(Newest Top, Oldest Top for now), and Drag and Drop Sorting
 //We will declare a global JavaScript Array Notes that we will interact with in all functions. 
@@ -109,4 +118,96 @@ function DeleteNote()
 });
 
 }
+
+//Code that follows if for drag and drop see example homepage.html on these parts fit together
+//Function drop appened to the HTML on drop event on the table, fill in parameter with event that should work
+function drop(ev) {
+    //starting at index loop through and push down in the array
+    
+    //send list to server with new order to save
+    //PUT REQUEST
+$.ajax({
+    url: '/notes',
+    dataType: "json",
+    type: "PUT",
+    contentType: 'text/plain; charset=utf-8',
+    data: JSON.stringify({note: Notes}),
+    async: true,
+    processData: false,
+    cache: false,
+//need to change these to suit our functionality
+    success: function (data) {
+        alert(data);
+    },
+    error: function (xhr) {
+        alert('error');
+        //code to refresh webpage
+    }
+});
+    //copy the contents to their respective elements while looping through the array
+    //through in after the ajax call so that while the ajax is waiting on the server our webpage is reloading elements
+
+
+     ev.preventDefault();
+     var data = ev.dataTransfer.getData("text");
+     ev.target.appendChild(document.getElementById(data));
+ }
+
+//our JavaScript object that is being moved, first is set to the information in the table 
+var objectMoving;
+//variable we will use to keep track of our object
+var index;
+//Function drag set to ondragstart happens when we pick up the object
+ function drag(ev) {
+    //copy the current contents in table to a javascript variable
+    objectMoving = 
+    //grab the row number
+    index = getRowNumber(objectMoving) - 1;
+    //set the variable to the variable located in the list
+    alert("Is the Index that follows right?");
+    objectMoving = Notes[index];
+
+ }
+
+ //allowDrop function append this to drag on drag over
+function allowDrop(ev) {
+     ev.preventDefault();
+ }
+
+ //Our edit code will be called on button click
+
+ function edit(ev)
+ {
+    //get the row that the button is on
+    var row = getRowNumber(ev) - 1;
+    var noteForActions = Notes[row];
+    //turn the tag into a textbox and and load the contents from the existing tag into the text box
+
+
+    //pop up a finish box
+
+    //after clicking the finish box that load the finished text into the Note
+
+    //send an ajax request on success copy new text into its element in the array
+    $.ajax({
+    url: '/notes',
+    dataType: "json",
+    type: "PUT",
+    contentType: 'text/plain; charset=utf-8',
+    data: JSON.stringify({/* put our object we are updating here */}),
+    async: true,
+    processData: false,
+    cache: false,
+    //at success load the new noteForActions into the note list 
+    success: function (data) {
+        Notes[row] = noteForActions;
+    },
+    //at failure pop up another box to try again or close and revert text
+    error: function (xhr) {
+        alert('error');
+    }
+});
+
+ }
 </script>
+
